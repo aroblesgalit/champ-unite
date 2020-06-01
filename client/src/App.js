fdaae7b77def92f4d3bdf326634f4f7c9e74e41e
Alvin Galit <aroblesgalit@gmail.com>
2020-06-01 17:26:47 -0500

    Created a homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
