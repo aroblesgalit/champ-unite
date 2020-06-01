47dfa80e7394b8e73a77b3a914d0e504c58a1975
Alvin Galit <aroblesgalit@gmail.com>
2020-06-01 17:23:11 -0500

    Created header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header>
-            <nav>
-            
+            <nav className="uk-navbar-container uk-margin" uk-navbar>
+                <div className="uk-navbar-left">
+                    <Link className="uk-navbar-item uk-logo" to="/">ChampUnite</Link>
+                    <ul className="uk-navbar-nav">
+                        <li><Link to="">Ranking</Link></li>
+                        <li><Link to="">Users</Link></li>
+                        <li><Link to="">Champions</Link></li>
+                    </ul>
+                </div>
+                <div className="uk-navbar-right">
+                    <ul className="uk-navbar-nav">
+                        <li>
+                            <Link to="">Login</Link>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </header>
     );
