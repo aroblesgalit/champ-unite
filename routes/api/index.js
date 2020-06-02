efd81025d86b4840bac2affa493875e84bb83454
Alvin Galit <aroblesgalit@gmail.com>
2020-06-01 20:59:26 -0500

    Created routes for user

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const championRoutes = require("./champions");
+const userRoutes = require("./users");
 
 router.use("/champions", championRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,3 +5,30 @@ const router = require("express").Router();
 router.post("/login", passport.authenticate("local"), function(req, res) {
     res.redirect("/users/" + req.user.username)
 });
+
+router.post("/signup", function (req, res) {
+    db.User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(function (dbUser) {
+            res.redirect(307, "/api/users/login");
+        })
+        .catch(function (err) {
+            res.status(401).json(err);
+        })
+});
+
+router.get("/user_data", function (req, res) {
+    if (!req.user) {
+        res.status(401).json({});
+    } else {
+        res.json({
+            id: req.user._id,
+            username: req.user.username
+        })
+    }
+});
+
+module.exports = router;
