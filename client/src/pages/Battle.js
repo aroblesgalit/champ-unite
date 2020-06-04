7bbc39ed5bc58f776aa682195f4c7049f25957d8
Alvin Galit <aroblesgalit@gmail.com>
2020-06-04 11:04:20 -0500

    Added champion cards and start button to battle page

--- client/src/pages/Battle.js
+++ client/src/pages/Battle.js
@@ -1,14 +1,27 @@
 import React from "react";
 import HealthBar from "../components/HealthBar";
+import ChampionCard from "../components/ChampionCard";
 
 function Battle() {
     return (
         <section className="battle-container uk-flex uk-flex-column uk-flex-middle">
             <h2>BATTLE</h2>
             <div className="uk-flex uk-flex-middle uk-flex-between uk-width-expand">
-                <HealthBar type="user" />
-                <HealthBar type="otherUser" />
+                <div className="uk-flex uk-flex-column uk-flex-middle">
+                    <HealthBar type="user" />
+                    <ChampionCard />
+                    <span className="battle-user-name">Username</span>
+                </div>
+                <div className="battle-vs">
+                    <span>VS</span>
+                </div>
+                <div className="uk-flex uk-flex-column uk-flex-middle">
+                    <HealthBar type="otherUser" />
+                    <ChampionCard />
+                    <span className="battle-user-name">Username</span>
+                </div>
             </div>
+            <button className="uk-button primary-btn">Start</button>
         </section>
     );
 }
