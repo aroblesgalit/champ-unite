89144ffd80a89ed7ac4c6a10c1b07b941fd39704
Alvin Galit <aroblesgalit@gmail.com>
2020-06-09 22:07:05 -0500

    Made navbar responsive

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -38,7 +38,7 @@ function Header() {
     return (
         <header>
             <nav className="uk-navbar-container uk-navbar-transparent uk-light" uk-navbar="true">
-                <div className="uk-navbar-left">
+                <div className="nav-left uk-navbar-left">
                     <Link className="uk-navbar-item uk-logo" to="/">ChampUnite</Link>
                     <ul className="uk-navbar-nav">
                         <li><Link to="/Ranking">Ranking</Link></li>
@@ -46,7 +46,7 @@ function Header() {
                         <li><Link to="/champions">Champions</Link></li>
                     </ul>
                 </div>
-                <div className="uk-navbar-right">
+                <div className="nav-right-large uk-navbar-right">
                     {
                         user.isLoggedIn ? (
                             <ul className="uk-navbar-nav">
@@ -65,6 +65,32 @@ function Header() {
                                 </ul>
                             )
                     }
+                </div>
+                <div className="nav-right-small uk-navbar-right uk-margin-top uk-margin-right">
+                    <button className="nav-small-icon uk-button uk-button-default" type="button" uk-toggle="target: #nav-small"><span uk-icon="icon: menu"></span></button>
+                    <div id="nav-small" uk-offcanvas="flip: true; overlay: true">
+                        <div className="nav-bar-small uk-offcanvas-bar uk-flex uk-flex-column">
+                            <button class="uk-offcanvas-close" type="button" uk-close="true"></button>
+                            {
+                                user.isLoggedIn ? (
+                                    <ul className="uk-nav uk-nav-primary uk-nav-center">
+                                        <li><Link to="/Ranking">Ranking</Link></li>
+                                        <li><Link to="/users">Users</Link></li>
+                                        <li><Link to="/champions">Champions</Link></li>
+                                        <li><Link to="/profile">Profile</Link></li>
+                                        <li><Link to="/login" onClick={handleLogout} >Logout</Link></li>
+                                    </ul>
+                                ) : (
+                                        <ul className="uk-nav uk-nav-primary uk-nav-center">
+                                            <li><Link to="/Ranking">Ranking</Link></li>
+                                            <li><Link to="/users">Users</Link></li>
+                                            <li><Link to="/champions">Champions</Link></li>
+                                            <li><Link to="/login">Login</Link></li>
+                                        </ul>
+                                    )
+                            }
+                        </div>
+                    </div>
                 </div>
             </nav>
         </header>
