8bb8b2af2c1c8ddeef318905227beda5d373ff2f
Alvin Galit <aroblesgalit@gmail.com>
2020-07-14 17:39:10 -0500

    Created a feature card component

--- client/src/components/Features/index.js
+++ client/src/components/Features/index.js
@@ -12,10 +12,12 @@ function Features() {
             <h2>Features</h2>
             <div className="uk-margin-top">
                 <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
-                    <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={searchFeature} alt="Search feature" uk-img="true" />
+                    <dv className="feature-img uk-width-1-3@m uk-width-1-2@s">
+                        <img src={searchFeature} alt="Search feature" uk-img="true" />
+                    </dv>
                     <div className="feature-text uk-flex uk-flex-column">
                         <h4>Search Champions</h4>
-                        <p className="uk-margin-remove-top">Use the search feature to search for your 
+                        <p className="uk-margin-remove-top">Use the search feature to search for your
                         favorite superheroes/champions from a list of over 700.</p>
                     </div>
                 </div>
@@ -23,8 +25,8 @@ function Features() {
                     <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={createFeature} alt="Create feature" uk-img="true" />
                     <div className="feature-text uk-flex uk-flex-column">
                         <h4>Create a Champion</h4>
-                        <p className="uk-margin-remove-top">You have the freedom to create your own champion. 
-                        Just need a name and an image url. The powerstats will be randomly generated so test your 
+                        <p className="uk-margin-remove-top">You have the freedom to create your own champion.
+                        Just need a name and an image url. The powerstats will be randomly generated so test your
                         luck! The attack and defense will be calculated based on the powerstats.</p>
                     </div>
                 </div>
@@ -32,7 +34,7 @@ function Features() {
                     <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={selectFeature} alt="Select feature" uk-img="true" />
                     <div className="feature-text uk-flex uk-flex-column">
                         <h4>Select a Champion for Battle</h4>
-                        <p className="uk-margin-remove-top">Select a user to battle from the Users Search page and 
+                        <p className="uk-margin-remove-top">Select a user to battle from the Users Search page and
                         then select one of your champions to go into battle.</p>
                     </div>
                 </div>
@@ -40,9 +42,9 @@ function Features() {
                     <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={battleFeature} alt="Battle feature" uk-img="true" />
                     <div className="feature-text uk-flex uk-flex-column">
                         <h4>Battle Users</h4>
-                        <p className="uk-margin-remove-top">Watch as you do a one-on-one battle against another user's 
-                        champion. This is automatic and the champions take turns attacking one another. We take 
-                        the attacker's Attack value, randomize a value from 1 up to the champion's Defense value 
+                        <p className="uk-margin-remove-top">Watch as you do a one-on-one battle against another user's
+                        champion. This is automatic and the champions take turns attacking one another. We take
+                        the attacker's Attack value, randomize a value from 1 up to the champion's Defense value
                         in defense mode, and calculate the difference for the health damage.</p>
                     </div>
                 </div>
