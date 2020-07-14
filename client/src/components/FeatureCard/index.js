ac3087cd2f0cdc2a479da3892d495d304af1269f
Alvin Galit <aroblesgalit@gmail.com>
2020-07-14 17:46:05 -0500

    Used feature card in features component

--- client/src/components/FeatureCard/index.js
+++ client/src/components/FeatureCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./featureCard.css";
 
-function FeatureCard(title, description, img) {
+function FeatureCard({ title, description, img }) {
     return (
         <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
             <dv className="feature-img uk-width-1-3@m uk-width-1-2@s">
--- client/src/components/Features/index.js
+++ client/src/components/Features/index.js
@@ -5,56 +5,59 @@ import createFeature from "../../images/create-feature.png";
 import selectFeature from "../../images/select-feature.png";
 import battleFeature from "../../images/battle-feature.png";
 import rankingFeature from "../../images/ranking-feature.png";
+import FeatureCard from "../FeatureCard";
 
 function Features() {
+
+    const features = [
+        {
+            id: 1,
+            title: "Search Champions",
+            description: "Use the search feature to search for your favorite superheroes/champions from a list of over 700.",
+            img: searchFeature
+        },
+        {
+            id: 2,
+            title: "Create a Champion",
+            description: "You have the freedom to create your own champion. Just need a name and an image url. The powerstats will be randomly generated so test your luck! The attack and defense will be calculated based on the powerstats.",
+            img: createFeature
+        },
+        {
+            id: 3,
+            title: "Select a Champion for Battle",
+            description: "Select a user to battle from the Users Search page and then select one of your champions to go into battle.",
+            img: selectFeature
+        },
+        {
+            id: 4,
+            title: "Battle Users",
+            description: "Watch as you do a one-on-one battle against another user's champion. This is automatic and the champions take turns attacking one another. We take the attacker's Attack value, randomize a value from 1 up to the champion's Defense value in defense mode, and calculate the difference for the health damage.",
+            img: battleFeature
+        },
+        {
+            id: 5,
+            title: "Make the Ranking",
+            description: "Battle at least 20 times to get on the ranking with the bests.",
+            img: rankingFeature
+        }
+    ];
+
     return (
         <section className="features-container uk-section uk-flex uk-flex-column uk-flex-middle">
             <h2>Features</h2>
             <div className="uk-margin-top">
-                <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
-                    <dv className="feature-img uk-width-1-3@m uk-width-1-2@s">
-                        <img src={searchFeature} alt="Search feature" uk-img="true" />
-                    </dv>
-                    <div className="feature-text uk-flex uk-flex-column">
-                        <h4>Search Champions</h4>
-                        <p className="uk-margin-remove-top">Use the search feature to search for your
-                        favorite superheroes/champions from a list of over 700.</p>
-                    </div>
-                </div>
-                <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
-                    <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={createFeature} alt="Create feature" uk-img="true" />
-                    <div className="feature-text uk-flex uk-flex-column">
-                        <h4>Create a Champion</h4>
-                        <p className="uk-margin-remove-top">You have the freedom to create your own champion.
-                        Just need a name and an image url. The powerstats will be randomly generated so test your
-                        luck! The attack and defense will be calculated based on the powerstats.</p>
-                    </div>
-                </div>
-                <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
-                    <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={selectFeature} alt="Select feature" uk-img="true" />
-                    <div className="feature-text uk-flex uk-flex-column">
-                        <h4>Select a Champion for Battle</h4>
-                        <p className="uk-margin-remove-top">Select a user to battle from the Users Search page and
-                        then select one of your champions to go into battle.</p>
-                    </div>
-                </div>
-                <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
-                    <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={battleFeature} alt="Battle feature" uk-img="true" />
-                    <div className="feature-text uk-flex uk-flex-column">
-                        <h4>Battle Users</h4>
-                        <p className="uk-margin-remove-top">Watch as you do a one-on-one battle against another user's
-                        champion. This is automatic and the champions take turns attacking one another. We take
-                        the attacker's Attack value, randomize a value from 1 up to the champion's Defense value
-                        in defense mode, and calculate the difference for the health damage.</p>
-                    </div>
-                </div>
-                <div className="uk-child-width-expand@s uk-text-left@s uk-text-center" uk-grid="true">
-                    <img className="feature-img uk-width-1-3@m uk-width-1-2@s" src={rankingFeature} alt="Ranking feature" uk-img="true" />
-                    <div className="feature-text uk-flex uk-flex-column">
-                        <h4>Make the Ranking</h4>
-                        <p className="uk-margin-remove-top">Battle at least 20 times to get on the ranking with the bests.</p>
-                    </div>
-                </div>
+                {
+                    features.map(feature => {
+                        return (
+                            <FeatureCard 
+                                key={feature.id}
+                                title={feature.title}
+                                description={feature.description}
+                                img={feature.img}
+                            />
+                        )
+                    })
+                }
             </div>
         </section>
     );
