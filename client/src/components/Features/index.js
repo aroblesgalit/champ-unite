5dbb8818fd920b79a517b90743e1f520940ece19
Alvin Galit <aroblesgalit@gmail.com>
2020-07-15 23:12:31 -0500

    Added new feature

--- client/src/components/Features/index.js
+++ client/src/components/Features/index.js
@@ -5,37 +5,38 @@ import createFeature from "../../images/create-feature.png";
 import selectFeature from "../../images/select-feature.png";
 import battleFeature from "../../images/battle-feature.png";
 import rankingFeature from "../../images/ranking-feature.png";
+import usersFeature from "../../images/users-feature.png";
 import FeatureCard from "../FeatureCard";
 
 function Features() {
 
     const features = [
         {
-            id: 1,
             title: "Search Champions",
             description: "Use the search feature to search for your favorite superheroes/champions from a list of over 700.",
             img: searchFeature
         },
         {
-            id: 2,
+            title: "Search Users",
+            description: "Browse through the Users page and check out other users and their champions. Go to their profile or battle their champions.",
+            img: usersFeature
+        },
+        {
             title: "Create a Champion",
             description: "You have the freedom to create your own champion. Just need a name and an image url. The powerstats will be randomly generated so test your luck! The attack and defense will be calculated based on the powerstats.",
             img: createFeature
         },
         {
-            id: 3,
             title: "Select a Champion for Battle",
-            description: "Select a user to battle from the Users Search page and then select one of your champions to go into battle.",
+            description: "You have the option to select one of your champions to go into battle with.",
             img: selectFeature
         },
         {
-            id: 4,
             title: "Battle Users",
             description: "Watch as you do a one-on-one battle against another user's champion. This is automatic and the champions take turns attacking one another. We take the attacker's Attack value, randomize a value from 1 up to the champion's Defense value in defense mode, and calculate the difference for the health damage.",
             img: battleFeature
         },
         {
-            id: 5,
             title: "Make the Ranking",
             description: "Battle at least 20 times to get on the ranking with the bests.",
             img: rankingFeature
@@ -47,10 +48,10 @@ function Features() {
             <h2>Features</h2>
             <div className="uk-margin-top">
                 {
-                    features.map(feature => {
+                    features.map((feature, index) => {
                         return (
                             <FeatureCard 
-                                key={feature.id}
+                                key={index}
                                 title={feature.title}
                                 description={feature.description}
                                 img={feature.img}
