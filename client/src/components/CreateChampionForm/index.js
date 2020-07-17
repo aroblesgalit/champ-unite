51bd523b3628d672328ad06c154292d3deb30da8
Alvin Galit <aroblesgalit@gmail.com>
2020-07-17 00:34:52 -0500

    Added image preview to create form

--- client/src/components/CreateChampionForm/index.js
+++ client/src/components/CreateChampionForm/index.js
@@ -29,53 +29,61 @@ function CreateChampionForm() {
     };
 
     return (
-        <form className="create-champion-form uk-flex uk-flex-column uk-flex-middle uk-height-1-1">
-            <div className="uk-margin-small uk-width-expand">
-                <div className="uk-inline uk-width-expand">
-                    <span className="uk-form-icon" uk-icon="icon: user"></span>
-                    <input
-                        className="uk-input"
-                        type="text"
-                        placeholder="champion_name"
-                        ref={nameRef}
-                        value={champion.name}
-                        onChange={() => setChampion({
-                            ...champion,
-                            name: nameRef.current.value
-                        })}
-                    />
+        <form className="create-champion-form uk-child-width-1-2@m uk-height-1-1 uk-grid">
+            <div>
+                <div className="uk-margin-small uk-width-expand">
+                    <div className="uk-inline uk-width-expand">
+                        <span className="uk-form-icon" uk-icon="icon: user"></span>
+                        <input
+                            className="uk-input"
+                            type="text"
+                            placeholder="champion_name"
+                            ref={nameRef}
+                            value={champion.name}
+                            onChange={() => setChampion({
+                                ...champion,
+                                name: nameRef.current.value
+                            })}
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="uk-margin-small uk-width-expand">
-                <div className="uk-inline uk-width-expand">
-                    <span className="uk-form-icon" uk-icon="icon: image"></span>
-                    <input
-                        className="uk-input"
-                        type="text"
-                        placeholder="image_url"
-                        ref={imageRef}
-                        value={champion.image}
-                        onChange={() => setChampion({
-                            ...champion,
-                            image: imageRef.current.value
-                        })}
-                    />
+                <div className="uk-margin-small uk-width-expand">
+                    <div className="uk-inline uk-width-expand">
+                        <span className="uk-form-icon" uk-icon="icon: reddit"></span>
+                        <input
+                            className="uk-input"
+                            type="text"
+                            placeholder="race"
+                            ref={raceRef}
+                            value={champion.race}
+                            onChange={() => setChampion({
+                                ...champion,
+                                race: raceRef.current.value
+                            })}
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="uk-margin-small uk-width-expand">
-                <div className="uk-inline uk-width-expand">
-                    <span className="uk-form-icon" uk-icon="icon: reddit"></span>
-                    <input
-                        className="uk-input"
-                        type="text"
-                        placeholder="race"
-                        ref={raceRef}
-                        value={champion.race}
-                        onChange={() => setChampion({
-                            ...champion,
-                            race: raceRef.current.value
-                        })}
-                    />
+                <div className="uk-margin-small uk-width-expand">
+                    <div className="uk-inline uk-width-expand">
+                        <span className="uk-form-icon" uk-icon="icon: image"></span>
+                        <input
+                            className="uk-input"
+                            type="text"
+                            placeholder="image_url"
+                            ref={imageRef}
+                            value={champion.image}
+                            onChange={() => setChampion({
+                                ...champion,
+                                image: imageRef.current.value
+                            })}
+                        />
+                    </div>
+                </div>
+                <div className="image-prev uk-margin-top">
+                    {
+                        champion.image ? <img src={champion.image} alt="Preview" /> : <p className="uk-text-small uk-text-mute">image preview</p>
+                    }
+
                 </div>
             </div>
             <UserConsumer>
