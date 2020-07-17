35438d7ec31f25f2e7e11373fdfa791fffc6e0a9
Alvin Galit <aroblesgalit@gmail.com>
2020-07-17 15:19:30 -0500

    Updated profile header for mobile view

--- client/src/components/ProfileHeader/index.js
+++ client/src/components/ProfileHeader/index.js
@@ -14,51 +14,53 @@ function ProfileHeader(props) {
     const { displayName, username, rank, wins, losses, champions, image, type } = props;
 
     return (
-        <section className="profile-header uk-section uk-flex uk-flex-middle uk-light">
-            <div className="profile-img-container uk-flex uk-flex-center uk-flex-middle uk-margin-right uk-position-relative">
-                {
-                    info.username === username ? (
-                        <span
-                            uk-icon="image"
-                            className="uk-position-absolute uk-icon-button"
-                            onClick={() => handleImageModal()}
-                        />
-                    ) : ""
-                }
-                <img src={image} alt="Avatar" />
-            </div>
-            <div className="user-stats-wrapper uk-flex uk-flex-bottom">
-                <div className="uk-flex uk-flex-column uk-margin-large-right">
-                    <h2>{displayName}</h2>
-                    <div className="uk-flex">
-                        <div className="user-stats uk-flex uk-flex-column uk-flex-middle">
-                            <span className="stat-val">{rank === 0 ? "-" : rank}</span>
-                            <span className="stat-label">Rank</span>
-                        </div>
-                        <div className="user-stats uk-flex uk-flex-column uk-flex-middle">
-                            <span className="stat-val">{wins}</span>
-                            <span className="stat-label">Wins</span>
-                        </div>
-                        <div className="user-stats uk-flex uk-flex-column uk-flex-middle">
-                            <span className="stat-val">{losses}</span>
-                            <span className="stat-label">Losses</span>
+        <section className="profile-header uk-section uk-flex uk-flex-bottom uk-light">
+            <div className="img-and-stats uk-flex uk-flex-middle">
+                <div className="profile-img-container uk-flex uk-flex-center uk-flex-middle uk-margin-right uk-position-relative">
+                    {
+                        info.username === username ? (
+                            <span
+                                uk-icon="image"
+                                className="uk-position-absolute uk-icon-button"
+                                onClick={() => handleImageModal()}
+                            />
+                        ) : ""
+                    }
+                    <img src={image} alt="Avatar" />
+                </div>
+                <div className="user-stats-wrapper uk-flex uk-flex-bottom">
+                    <div className="uk-flex uk-flex-column uk-margin-large-right">
+                        <h2>{displayName}</h2>
+                        <div className="uk-flex">
+                            <div className="user-stats uk-flex uk-flex-column uk-flex-middle">
+                                <span className="stat-val">{rank === 0 ? "-" : rank}</span>
+                                <span className="stat-label">Rank</span>
+                            </div>
+                            <div className="user-stats uk-flex uk-flex-column uk-flex-middle">
+                                <span className="stat-val">{wins}</span>
+                                <span className="stat-label">Wins</span>
+                            </div>
+                            <div className="user-stats uk-flex uk-flex-column uk-flex-middle">
+                                <span className="stat-val">{losses}</span>
+                                <span className="stat-label">Losses</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-                {
-                    type === "otherUser" && champions && info.champions && info.champions.length > 0 && champions.length > 0 ? (
-                        <button
-                            className="uk-button secondary-btn"
-                            onClick={() => {
-                                handleChampionSelect(champions);
-                                handleModal();
-                            }}
-                        >
-                            Battle
-                        </button>
-                    ) : ""
-                }
             </div>
+            {
+                type === "otherUser" && champions && info.champions && info.champions.length > 0 && champions.length > 0 ? (
+                    <button
+                        className="uk-button secondary-btn"
+                        onClick={() => {
+                            handleChampionSelect(champions);
+                            handleModal();
+                        }}
+                    >
+                        Battle
+                    </button>
+                ) : ""
+            }
         </section>
     );
 }
