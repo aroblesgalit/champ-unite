dc2b988872c164f93f0cb87a530065868fb62e71
Alvin Galit <aroblesgalit@gmail.com>
2020-07-18 16:19:40 -0500

    Updated champion select modal to use slider

--- client/src/components/ChampSelectModal/index.js
+++ client/src/components/ChampSelectModal/index.js
@@ -23,31 +23,33 @@ function ChampSelectModal() {
                                     <div className="user-champions-modal-header">
                                         <p>Select one of your champions to go into battle.</p>
                                     </div>
-                                    <div className="user-champions-modal-body uk-flex uk-flex-wrap">
-                                        {
-                                            info.champions && info.champions.length > 0 ? (
-                                                info.champions.map(champion => {
-                                                    return <ChampionCard
-                                                        key={champion._id || champion.image}
-                                                        id={champion._id}
-                                                        name={champion.name}
-                                                        image={champion.image}
-                                                        strength={champion.strength}
-                                                        power={champion.power}
-                                                        combat={champion.combat}
-                                                        intelligence={champion.intelligence}
-                                                        speed={champion.speed}
-                                                        durability={champion.durability}
-                                                        attack={champion.attack}
-                                                        defense={champion.defense}
-                                                        type="battle"
-                                                        handleSelect={() => handleSelect(champion._id)}
-                                                        selected={championSelected}
-                                                        selectedId={selectedId}
-                                                    />
-                                                })
-                                            ) : <p>Search for Champions to add or create your own!</p>
-                                        }
+                                    <div className="user-champions-modal-body uk-position-relative uk-visible-toggle" tabIndex="-1" uk-slider="sets: true">
+                                        <div className="uk-flex uk-flex-nowrap uk-slider-items">
+                                            {
+                                                info.champions && info.champions.length > 0 ? (
+                                                    info.champions.map(champion => {
+                                                        return <ChampionCard
+                                                            key={champion._id || champion.image}
+                                                            id={champion._id}
+                                                            name={champion.name}
+                                                            image={champion.image}
+                                                            strength={champion.strength}
+                                                            power={champion.power}
+                                                            combat={champion.combat}
+                                                            intelligence={champion.intelligence}
+                                                            speed={champion.speed}
+                                                            durability={champion.durability}
+                                                            attack={champion.attack}
+                                                            defense={champion.defense}
+                                                            type="battle"
+                                                            handleSelect={() => handleSelect(champion._id)}
+                                                            selected={championSelected}
+                                                            selectedId={selectedId}
+                                                        />
+                                                    })
+                                                ) : <p>Search for Champions to add or create your own!</p>
+                                            }
+                                        </div>
                                     </div>
                                     <div className="user-champions-modal-footer uk-text-right">
                                         <button className="uk-button outline-btn uk-modal-close uk-margin-small-right" type="button" onClick={() => handleModal()}>Cancel</button>
